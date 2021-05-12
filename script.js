d173ce3ed36d1404b076478fbf3d0cbc0ffba60b
Jessica Washington <jessicawashington@Jessicas-MBP.attlocal.net>
2021-05-12 16:21:08 -0400

    creating js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const baseURL = 'https://pokeapi.co/api/v2/pokemon/'
+const baseURL = 'https://pokeapi.co/api/v2/'
 
 const searchForm = document.querySelector('form');
 const locationArea = document.querySelector('section');
 const searchTerm = document.querySelector('.search')
 searchForm.addEventListener('submit', fetchLocationArea)
+
 console.log(baseURL);
+
 function fetchLocationArea(event) {
     event.preventDefault();
-    url = `${baseURL}/${searchTerm.value}/encounters`
+    url = `${baseURL}pokemon/${searchTerm.value}/encounters`
     console.log(baseURL);
     fetch(url)
         .then(results => {
@@ -15,14 +17,21 @@ function fetchLocationArea(event) {
             return results.json();
         })
         .then(json => {
-            displayLocationArea(json);
+            console.log(json);
+            displayResults(json);
         })
         .catch(err => console.log(err));
 
 }
 
-function displayResults(json){
-    console.log('Display Results', json);
-}
+function displayResults(LocationArea){
+    console.log('Display Results', LocationArea);
+    for(let i = 0; i < LocationArea.length; i++){
+        let location = document.createElement('li');
+        let current = LocationArea[i];
+        location.innerText = current.location_area.name;
+        locationArea.appendChild(location);
+    }
 
+}
 //fetchLocationArea();
